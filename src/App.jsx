547c7c5084a05b17ade83878c550e8914c3ecd81
Razieh <razie.ahmadi93gmail.com>
2024-07-25 10:36:36 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Register from "./Register.jsx";
+
+function App() {
+  return (
+    <main className="App">
+      <Register />
+    </main>
+  );
+}
+
+export default App;

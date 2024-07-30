d3bfb540a94e148a19fec4bded8218e4aa8cdb13
Razieh <razie.ahmadi93@gmail.com>
2024-07-30 14:50:49 -0700

    Add Initial Implementation of Login Component

---
+++ src/Login.jsx
@@ -0,0 +1,22 @@
+import { useRef, useState, useEffect } from "react";
+
+function Login() {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+  return <div></div>;
+}
+
+export default Login;

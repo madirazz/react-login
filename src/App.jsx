c8b6f9231ab6f5bfa4833d20ec68499b303dce8e
madirazz <razie.ahmadi93@gmail.com>
2024-07-30 17:31:51 -0700

    add Log in route to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./Register.jsx";
+import Login from "./Login.jsx";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );

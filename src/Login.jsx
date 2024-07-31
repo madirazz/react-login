3241e03d6221c90e1b68efdfa8525e245345e052
madirazz <razie.ahmadi93@gmail.com>
2024-07-30 17:34:27 -0700

    Add sign in form

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState, useEffect } from "react";
+import axios from "axios";
 
 function Login() {
   const userRef = useRef();
@@ -16,7 +17,64 @@ function Login() {
   useEffect(() => {
     setErrMsg("");
   }, [user, pwd]);
-  return <div></div>;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3000/login", {
+        username: user,
+        password: pwd,
+      });
+      console.log(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <section>
+        <p
+          ref={errRef}
+          className={errMsg ? "errmsg" : "offscreen"}
+          aria-live="assertive"
+        >
+          {errMsg}
+        </p>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            ref={userRef}
+            autoComplete="off"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
+          />
+
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            required
+          />
+
+          <button>Log in</button>
+        </form>
+        <p>
+          Need an account?
+          <br />
+          <span className="line">
+            <a href="#">Sign up</a>
+          </span>
+        </p>
+      </section>
+    </div>
+  );
 }
 
 export default Login;
